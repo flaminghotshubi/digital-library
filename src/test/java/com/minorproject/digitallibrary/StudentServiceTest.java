37b3976c615394e0e546538dd6cccff3f9b79bb0
Shubhangi Agarwal <shubhi.agarwal019@gmail.com>
2024-03-04 21:46:36 +0530

    Mockito and junit code added

--- src/test/java/com/minorproject/digitallibrary/StudentServiceTest.java
+++ src/test/java/com/minorproject/digitallibrary/StudentServiceTest.java
@@ -1,21 +1,36 @@
 package com.minorproject.digitallibrary;
 
+import com.minorproject.digitallibrary.dto.StudentResponse;
+import com.minorproject.digitallibrary.model.Student;
+import com.minorproject.digitallibrary.repository.StudentRepository;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 
 import com.minorproject.digitallibrary.service.StudentService;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
 
 @RunWith(MockitoJUnitRunner.class) // how you want to execute your test cases
 public class StudentServiceTest {
 	
-	@InjectMocks
+	@InjectMocks // similar to autowired
 	StudentService studentService;
-	
+
+    @Mock
+    StudentRepository studentRepo;
+
 	@Test
 	public void getById_test() {
-		studentService.getById(5);
+        Student s = Student.builder().id(99).name("dummy").contact("100").build();
+        Mockito.when(studentRepo.findById(Mockito.anyInt())).thenReturn(Optional.of(s));
+		StudentResponse res = studentService.getById(99);
+		Assert.assertEquals(s.getId(), res.getId());
+		Mockito.verify(studentRepo, Mockito.times(1)).findById(99);
 	}
 
 }
